c66a622ff6bf07360be4cd9c5639a95b8c5c0585
rgustavo03 <129887015+rgustavo03@users.noreply.github.com>
2023-04-24 11:51:34 -0300

    Update connect.php

--- php/connect.php
+++ php/connect.php
@@ -1,10 +1,22 @@
 <!-- Codigo da estrutura da tabela -->
 
-<!-- CREATE TABLE `tarefas` (`id` INT NOT NULL AUTO_INCREMENT , 
-`nome_tarefa` VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL , 
-`hora_tarefa` TIME NOT NULL , 
-`estado_tarefa` VARCHAR(2) NOT NULL DEFAULT 'A' , 
-PRIMARY KEY (`id`)) ENGINE = InnoDB; -->
+<!--
+CREATE TABLE usuario (
+    id_usuario int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nome varchar(250) NOT NULL,
+    email varchar(100) NOT NULL,
+    senha varchar(16) NOT NULL) ENGINE = InnoDB;
+-->
+
+<!--
+CREATE TABLE tarefas (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, 
+    nome_tarefa VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+    hora_tarefa TIME NOT NULL,
+    estado_tarefa VARCHAR(2) NOT NULL DEFAULT 'A',
+    id_usuario_fk int(8),
+    CONSTRAINT tarefa_usuario FOREIGN KEY (id_usuario_fk) REFERENCES usuario (id_usuario)) ENGINE = InnoDB;
+-->
 
 <?php
 //Configuração da conexão com o banco de dados
